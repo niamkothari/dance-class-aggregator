77d685a57e521a62a6a8867bddbc2f1e5510e341
Niam Kothari <niamkothari@Niams-MacBook-Pro-2.local>
2023-06-15 11:30:30 -0400

    basic ui setup

---
+++ ui/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>Welcome to Dance Class Aggregator</div>
+        <div @click="getJaviClass()">Click to see Sam Javi's next class in the browser console</div>
+    </div>
+</template>
+<script setup lang="ts">
+
+async function getJaviClass() {
+    const response = await fetch("/api/class")
+    const data = await response.json()
+    console.log("Javi data received")
+    console.log(data.name)
+}
+</script>
---
+++ ui/vite.config.js
@@ -0,0 +1,28 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'@': `${path.resolve(__dirname, 'src')}`,
+		},
+	},
+
+	build: {
+		minify: true,
+	},
+
+	plugins: [
+		vue(),
+	],
+
+	server: {
+		port: 8094,
+		proxy: {
+			"^/api": {
+				target: "http://localhost:8095"
+			}
+		}
+	},
+})
