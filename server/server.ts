49c115cb6f4f2d46e4f6ed74b6c6b9f84263dbe6
Niam Kothari <niamkothari@Niams-MacBook-Pro-2.local>
2023-08-09 13:46:15 -0400

    new endpoint

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import pino from 'pino'
-import expressPinoLogger from 'express-pino-logger'
+import expressPinotLogger from 'express-pino-logger'
 import { classes, studios } from './data'
+import { Class } from './data'
+import { Collection, Db, MongoClient, ObjectId } from 'mongodb'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import { Issuer, Strategy } from 'openid-client'
+import passport from 'passport'
+import { runScraper, fetchData, parseHTML, extractClassesInfo, extractStartTime, extractEndTimes } from './scraper'
 
 
 const app = express()
@@ -10,12 +17,12 @@ const port = 8095
 app.use(bodyParser.json())
 
 // set up Pino logging
-const logger = pino({
-    transport: {
-        target: 'pino-pretty'
-    }
-})
-app.use(expressPinoLogger({ logger }))
+// const logger = pino({
+//     transport: {
+//         target: 'pino-pretty'
+//     }
+// })
+// app.use(expressPinotLogger({ logger }))
 
 // start server
 app.listen(port, () => {
@@ -33,4 +40,10 @@ app.get("/api/studios", (req, res) => {
     // query db to get all studios
 
     res.status(200).json(studios)
+})
+
+app.post("/api/new-class", (req, res) => {
+    const newClass = req.body
+    // send to db
+    res.status(200).json({status: 'ok'})
 })
