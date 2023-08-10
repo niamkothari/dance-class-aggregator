8081d35ac2ca0778757e8f09cd4f5bf482d30985
Niam Kothari <niamkothari@Niams-MacBook-Pro-2.local>
2023-08-10 00:25:58 -0400

    Routes for different screens, Vue Router dependency

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,19 +1,44 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from '@/App.vue'
-
+import HomeScreen from '@/views/HomeScreen.vue'
+import ClassesScreen from '@/views/ClassesScreen.vue'
+import AdminScreen from '@/views/AdminScreen.vue'
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue"
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: '/classes',
+      name: 'Classes',
+      component: ClassesScreen
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: AdminScreen
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeScreen
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   render: h => h(App),
 })
